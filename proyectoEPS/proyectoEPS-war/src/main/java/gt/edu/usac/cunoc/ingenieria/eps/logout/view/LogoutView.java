28a8cbe9896cd76f7e66ab21787df453f2241412
Crystian López <cryraf2095@gmail.com>
2020-06-11 16:58:19 -0600

    Integración Primera Fase

--- proyectoEPS/proyectoEPS-war/src/main/java/gt/edu/usac/cunoc/ingenieria/eps/logout/view/LogoutView.java
+++ proyectoEPS/proyectoEPS-war/src/main/java/gt/edu/usac/cunoc/ingenieria/eps/logout/view/LogoutView.java
@@ -1,7 +1,8 @@
-
 package gt.edu.usac.cunoc.ingenieria.eps.logout.view;
 
+import java.io.IOException;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletException;
@@ -10,14 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 @Named
 @RequestScoped
 public class LogoutView {
-   @Inject
-   private HttpServletRequest request;
 
-   
-   public String logout()
-           throws ServletException {
-       request.logout();
-       request.getSession().invalidate();
-       return "login";
-   }
+    @Inject
+    private HttpServletRequest request;
+    
+    @Inject
+    private ExternalContext externalContext;
+    
+    public String logout()
+            throws ServletException {
+        request.logout();
+        request.getSession().invalidate();
+        return "login";
+    }
+    
+    public void goToMyProfile() throws IOException{
+        externalContext.redirect(externalContext.getRequestContextPath() + "/user/userProfile.xhtml");
+    }
 }

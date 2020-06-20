9aafd5ee3b301419e57dc475c06205229f660337
Crystian López <cryraf2095@gmail.com>
2020-06-19 18:49:46 -0600

    accesos de seguridad para Supervisor_EPS y Coordinador_EPS a la pantalla de procesos

--- proyectoEPS/proyectoEPS-war/src/main/java/gt/edu/usac/cunoc/ingenieria/eps/committeeEPS/view/tailCommitteeEPSView.java
+++ proyectoEPS/proyectoEPS-war/src/main/java/gt/edu/usac/cunoc/ingenieria/eps/committeeEPS/view/tailCommitteeEPSView.java
@@ -1,9 +1,13 @@
 
 package gt.edu.usac.cunoc.ingenieria.eps.committeeEPS.view;
 
+import User.exception.UserException;
 import gt.edu.usac.cunoc.ingenieria.eps.process.Process;
 import gt.edu.usac.cunoc.ingenieria.eps.tail.facade.TailCommitteeEPSFacadeLocal;
+import gt.edu.usac.cunoc.ingenieria.eps.user.User;
+import gt.edu.usac.cunoc.ingenieria.eps.user.facade.UserFacadeLocal;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -17,8 +21,15 @@ public class tailCommitteeEPSView implements Serializable{
     @EJB
     private TailCommitteeEPSFacadeLocal tailCommitteeEPSFacade;
     
+    @EJB
+    private UserFacadeLocal userFacade;
+        
     private List<Process> processes;
 
+    private User user;
+    
+    private Boolean epsCommittee;
+    
     public List<Process> getProcesses() {
         return processes;
     }
@@ -27,10 +38,27 @@ public class tailCommitteeEPSView implements Serializable{
         this.processes = processes;
     }
 
+    public Boolean getEpsCommittee() {
+        return epsCommittee;
+    }
+
+    public void setEpsCommittee(Boolean epsCommittee) {
+        this.epsCommittee = epsCommittee;
+    }
+
     @PostConstruct
     public void init(){
-        this.processes = tailCommitteeEPSFacade.getTailCommitteeEPS();
+        try {
+            user = userFacade.getAuthenticatedUser().get(0);
+            this.epsCommittee = user.getEpsCommittee();
+            if (epsCommittee){
+                this.processes = tailCommitteeEPSFacade.getTailCommitteeEPS();
+            } else {
+                this.processes = new ArrayList<>();
+            }
+        } catch (UserException ex) {
+            System.out.println("Error Autenticación");
+        }
     }
-    
     
 }
